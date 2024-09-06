1f7c9b4ec1c0aee86bd9ea02f1de1d0d9b5f31a4
Micah Ma <701004018@GoTVDSB.ca>
2024-09-06 08:35:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Part_4_Random_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is a random number generator!(Press \'ENTER\' to start)");
+            Console.ReadLine();
+            Random random = new Random();
+            int min, max,digit;
+            do
+            {
+                Console.WriteLine("Please enter the minimum value!(integer)");
+            }
+            while (!int.TryParse(Console.ReadLine(), out min));
+            do
+            {
+                Console.WriteLine("Please enter the maximum value!(integer)");
+            }
+            while (!int.TryParse(Console.ReadLine(), out max)||!(max>=min));
+            do
+            {
+                Console.WriteLine("Please enter how many decimal place you would like!(integer)");
+            }
+            while (!int.TryParse(Console.ReadLine(), out digit));
+            Console.WriteLine(Math.Round(random.Next(min,max)+random.NextDouble(),digit));
+        }
+    }
+}
